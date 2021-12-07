d1dd4153048eb77430a453235f83329f585d1da2
meloidae <meloidaee@gmail.com>
2021-12-07 12:01:03 +0900

    Fix tuple2list in tuple_memory
    read_binary() was appending an extra byte

--- lib/gameboy/tuple_memory.ex
+++ lib/gameboy/tuple_memory.ex
@@ -9,12 +9,11 @@ defmodule Gameboy.TupleMemory do
   def read(data, addr), do: elem(data, addr)
 
   def read_binary(data, addr, len) do
-    tuple2list(data, addr, addr + len)
+    tuple2list(data, addr, addr + len - 1)
     |> IO.iodata_to_binary()
   end
 
   defp tuple2list(t, first, last), do: tuple2list(t, first, last, [])
-  defp tuple2list(_t, first, i, acc) when i < first, do: acc
+  defp tuple2list(t, first, first, acc), do: [elem(t, first) | acc]
   defp tuple2list(t, first, i, acc), do: tuple2list(t, first, i - 1, [elem(t, i) | acc])
-
 end
