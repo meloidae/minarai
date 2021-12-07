b5653fba4e19173c6ed50470037754bd98614395
meloidae <meloidaee@gmail.com>
2021-12-07 12:42:49 +0900

    Use MapMemory in wram & hram
    Significantly improved performance in the start screen!!

--- lib/gameboy/hram.ex
+++ lib/gameboy/hram.ex
@@ -1,22 +1,20 @@
 defmodule Gameboy.Hram do
   use Bitwise
   alias Gameboy.Hram
-  alias Gameboy.Memory
+  alias Gameboy.MapMemory
 
-  defstruct memory: struct(Memory)
+  defstruct memory: nil
 
   @hram_mask 0x007f
 
   def init do
-    memory = Memory.init(0x80)
+    memory = MapMemory.init(0x80)
     %Hram{memory: memory}
   end
 
-  def read(%Hram{memory: memory} = hram, addr), do: Memory.read(memory, addr &&& @hram_mask)
+  def read(%Hram{memory: memory} = _hram, addr), do: MapMemory.read(memory, addr &&& @hram_mask)
 
   def write(%Hram{memory: memory} = hram, addr, value) do
-    Map.put(hram, :memory, Memory.write(memory, addr &&& @hram_mask, value))
+    Map.put(hram, :memory, MapMemory.write(memory, addr &&& @hram_mask, value))
   end
-
-
 end
--- lib/gameboy/wram.ex
+++ lib/gameboy/wram.ex
@@ -1,38 +1,38 @@
 defmodule Gameboy.Wram do
   use Bitwise
   alias Gameboy.Wram
-  alias Gameboy.Memory
+  alias Gameboy.MapMemory
 
-  defstruct memory: struct(Memory),
+  defstruct memory: nil,
             offset: 0x1000 # CGB has switchable 1-7 high banks, but ignore that for now
 
   @wram_mask 0x0fff
 
   def init do
-    memory = Memory.init(0x8000)
+    memory = MapMemory.init(0x8000)
     %Wram{memory: memory}
   end
 
-  def read_low(%Wram{memory: memory} = wram, addr), do: Memory.read(memory, addr &&& @wram_mask)
+  def read_low(%Wram{memory: memory} = _wram, addr), do: MapMemory.read(memory, addr &&& @wram_mask)
 
-  def read_high(%Wram{memory: memory, offset: offset} = wram, addr) do
-    Memory.read(memory, offset ||| (addr &&& @wram_mask))
+  def read_high(%Wram{memory: memory, offset: offset} = _wram, addr) do
+    MapMemory.read(memory, offset ||| (addr &&& @wram_mask))
   end
 
-  def read_binary_low(%Wram{memory: memory} = wram, addr, len) do
-    Memory.read_binary(memory, addr &&& @wram_mask, len)
+  def read_binary_low(%Wram{memory: memory} = _wram, addr, len) do
+    MapMemory.read_binary(memory, addr &&& @wram_mask, len)
   end
 
-  def read_binary_high(%Wram{memory: memory, offset: offset} = wram, addr, len) do
-    Memory.read_binary(memory, offset ||| (addr &&& @wram_mask), len)
+  def read_binary_high(%Wram{memory: memory, offset: offset} = _wram, addr, len) do
+    MapMemory.read_binary(memory, offset ||| (addr &&& @wram_mask), len)
   end
 
   def write_low(%Wram{memory: memory} = wram, addr, value) do
-    Map.put(wram, :memory, Memory.write(memory, addr &&& @wram_mask, value))
+    Map.put(wram, :memory, MapMemory.write(memory, addr &&& @wram_mask, value))
   end
 
   def write_high(%Wram{memory: memory, offset: offset} = wram, addr, value) do
-    Map.put(wram, :memory, Memory.write(memory, offset ||| (addr &&& @wram_mask), value))
+    Map.put(wram, :memory, MapMemory.write(memory, offset ||| (addr &&& @wram_mask), value))
   end
 
 end
