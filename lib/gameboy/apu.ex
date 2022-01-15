e7cfb7a5945e4e277fd69bda2e622df9820c7fa6
meloidae <meloidaee@gmail.com>
2022-01-16 00:26:29 +0900

    Use MapMemory in apu

--- lib/gameboy/apu.ex
+++ lib/gameboy/apu.ex
@@ -1,21 +1,19 @@
 defmodule Gameboy.Apu do
   use Bitwise
   alias Gameboy.Apu
-  alias Gameboy.Memory
+  alias Gameboy.MapMemory
 
   # Just implement read/write for now. No real audio functionality
-  defstruct memory: struct(Memory)
 
   @apu_mask 0x3f
 
   def init do
-    memory = Memory.init(0x40)
-    %Apu{memory: memory}
+    MapMemory.init(0x40)
   end
 
-  def read(%{memory: memory} = apu, addr), do: Memory.read(memory, addr &&& @apu_mask)
+  def read(apu, addr), do: MapMemory.read(apu, addr &&& @apu_mask)
 
-  def write(%{memory: memory} = apu, addr, value) do
-    Map.put(apu, :memory, Memory.write(memory, addr &&& @apu_mask, value))
+  def write(apu, addr, value) do
+    MapMemory.write(apu, addr &&& @apu_mask, value)
   end
 end
