f39e145d1f352f2ce74dacdac93e978bb7f117e6
meloidae <meloidaee@gmail.com>
2022-11-10 19:27:34 +0900

    Use ets memory in apu

--- lib/gameboy/apu.ex
+++ lib/gameboy/apu.ex
@@ -1,19 +1,19 @@
 defmodule Gameboy.Apu do
   use Bitwise
   alias Gameboy.Apu
-  alias Gameboy.MapMemory
+  alias Gameboy.EtsMemory, as: RWMemory
 
   # Just implement read/write for now. No real audio functionality
 
   @apu_mask 0x3f
 
   def init do
-    MapMemory.init(0x40)
+    RWMemory.init(0x40, :apu)
   end
 
-  def read(apu, addr), do: MapMemory.read(apu, addr &&& @apu_mask)
+  def read(apu, addr), do: RWMemory.read(apu, addr &&& @apu_mask)
 
   def write(apu, addr, value) do
-    MapMemory.write(apu, addr &&& @apu_mask, value)
+    RWMemory.write(apu, addr &&& @apu_mask, value)
   end
 end
