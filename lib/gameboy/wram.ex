0ff503a6a29785d91d19a3fb2fb35278442abac6
meloidae <meloidaee@gmail.com>
2026-09-06 12:53:31 +0900

    Remove commented-out code from wram

--- lib/gameboy/wram.ex
+++ lib/gameboy/wram.ex
@@ -3,52 +3,31 @@ defmodule Gameboy.Wram do
   alias Gameboy.Wram
   alias Gameboy.AtomicsMemory, as: RWMemory
 
-  # defstruct memory: nil,
-  #           offset: 0x1000 # CGB has switchable 1-7 high banks, but ignore that for now
-
   @wram_mask 0x0fff
 
   def init do
-    # memory = MapMemory.init(0x2000)
-    # %Wram{memory: memory}
     {RWMemory.init(0x2000, :wram), 0x1000}
   end
 
-  # def read_low(%Wram{memory: memory} = _wram, addr), do: MapMemory.read(memory, addr &&& @wram_mask)
   def read_low({data, _offset}, addr), do: RWMemory.read(data, addr &&& @wram_mask)
 
-  # def read_high(%Wram{memory: memory, offset: offset} = _wram, addr) do
-  #   MapMemory.read(memory, offset ||| (addr &&& @wram_mask))
-  # end
   def read_high({data, offset}, addr) do
     RWMemory.read(data, offset ||| (addr &&& @wram_mask))
   end
 
-  # def read_binary_low(%Wram{memory: memory} = _wram, addr, len) do
-  #   MapMemory.read_binary(memory, addr &&& @wram_mask, len)
-  # end
   def read_binary_low({data, _offset}, addr, len) do
     RWMemory.read_binary(data, addr &&& @wram_mask, len)
   end
 
-  # def read_binary_high(%Wram{memory: memory, offset: offset} = _wram, addr, len) do
-  #   MapMemory.read_binary(memory, offset ||| (addr &&& @wram_mask), len)
-  # end
   def read_binary_high({data, offset}, addr, len) do
     RWMemory.read_binary(data, offset ||| (addr &&& @wram_mask), len)
   end
 
-  # def write_low(%Wram{memory: memory} = wram, addr, value) do
-  #   Map.put(wram, :memory, MapMemory.write(memory, addr &&& @wram_mask, value))
-  # end
   def write_low({data, _offset} = wram, addr, value) do
     RWMemory.write(data, addr &&& @wram_mask, value)
     wram
   end
 
-  # def write_high(%Wram{memory: memory, offset: offset} = wram, addr, value) do
-  #   Map.put(wram, :memory, MapMemory.write(memory, offset ||| (addr &&& @wram_mask), value))
-  # end
   def write_high({data, offset} = wram, addr, value) do
     RWMemory.write(data, offset ||| (addr &&& @wram_mask), value)
     wram
