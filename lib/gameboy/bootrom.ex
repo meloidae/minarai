ad23bba5d4b2a9f367d0296f1612454c99f5820f
meloidae <meloidaee@gmail.com>
2026-09-06 12:56:37 +0900

    Remove commented-out code from bootrom

--- lib/gameboy/bootrom.ex
+++ lib/gameboy/bootrom.ex
@@ -14,18 +14,13 @@ defmodule Gameboy.Bootrom do
       path
     end
     data = File.read!(path)
-    # %Bootrom{memory: %Memory{data: data}, active: true}
-    # %Bootrom{memory: TupleMemory.init(data), active: true}
     {TupleMemory.init(data), true}
   end
 
   def active({_memory, active} = _bootrom), do: active
 
   # Non-zero value disables bootrom
-  # def set_enable(bootrom, value), do: Map.put(bootrom, :active, value == 0)
   def set_enable({memory, _active}, value), do: {memory, value == 0}
 
-  # def read(%{memory: memory} = _bootrom, addr), do: Memory.read(memory, addr)
-  # def read(%{memory: memory} = _bootrom, addr), do: TupleMemory.read(memory, addr)
   def read({memory, _active} = _bootrom, addr), do: TupleMemory.read(memory, addr)
 end
