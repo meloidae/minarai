0f7914694df035575971963c3e382650b3fbdb95
meloidae <meloidaee@gmail.com>
2026-09-06 12:55:20 +0900

    Remove commented-out code from hram

--- lib/gameboy/hram.ex
+++ lib/gameboy/hram.ex
@@ -3,22 +3,14 @@ defmodule Gameboy.Hram do
   alias Gameboy.Hram
   alias Gameboy.AtomicsMemory, as: RWMemory
 
-  # defstruct memory: nil
-
   @hram_mask 0x007f
 
   def init do
-    # memory = MapMemory.init(0x80)
-    # %Hram{memory: memory}
     RWMemory.init(0x80, :hram)
   end
 
-  # def read(%Hram{memory: memory} = _hram, addr), do: MapMemory.read(memory, addr &&& @hram_mask)
   def read(hram, addr), do: RWMemory.read(hram, addr &&& @hram_mask)
 
-  # def write(%Hram{memory: memory} = hram, addr, value) do
-  #   Map.put(hram, :memory, MapMemory.write(memory, addr &&& @hram_mask, value))
-  # end
   def write(hram, addr, value) do
     RWMemory.write(hram, addr &&& @hram_mask, value)
   end
